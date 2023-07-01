32f5f3cc6e8fdd744811f87a50282edeb0e66d22
NurdauletMyrza <askar.nurdaulet1027@gmail.com>
2023-07-01 13:14:07 +0600

    app v1
    remove some files
    write code in app.js style.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+
+  
+  const decrement = () => setCount(count - 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="number">{count}</h1>
+      </div>
+
+      <section className="btns-container">
+        <button onClick={increment} className="increment">+</button>
+        <button onClick={decrement} className="decrement">-</button>
+      </section>
+    </>
   );
 }
 
